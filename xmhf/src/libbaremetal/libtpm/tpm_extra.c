321569e491b5de35c43f601905983dfb87f78872
Eric Li <xiaoyili@andrew.cmu.edu>
2022-03-29 00:12:03 -0400

    Reduce build command

--- xmhf/src/libbaremetal/libtpm/tpm_extra.c
+++ xmhf/src/libbaremetal/libtpm/tpm_extra.c
@@ -1,16 +1,20 @@
+// cd _build_libbaremetal/_objects
+// gcc -O3 -Wall -Werror   -c -o tpm_extra.o ../../xmhf/src/libbaremetal/libtpm/tpm_extra.c
+
 
 #include <stdint.h>
 
-typedef struct __attribute__ ((packed)) {
-    uint32_t                    enc_data_size;
+typedef struct {
+    uint32_t         enc_data_size;
 } tpm_short_t;
 
-typedef struct __attribute__ ((packed)) {
+typedef struct {
     uint16_t         tag;
-} tpm_pcr_info_long_t;
+} tpm_pcr_t;
 
-typedef struct __attribute__ ((packed)) {
-    tpm_pcr_info_long_t         seal_info;
+typedef struct {
+    tpm_pcr_t        seal_info;
+    uint16_t         tag2;
 } tpm_t;
 
 
@@ -22,24 +26,16 @@ void _tpm_seal(uint8_t *sealed_data)
 	{
 	   const uint8_t *p1 = rsp_buf;
 	   if ( (*(uint32_t *)(sealed_data)) == 12 ) {
-			uint8_t *p2 = (uint8_t *)&(((tpm_short_t *)sealed_data)->enc_data_size);
-			// p2[0] = p1[0];
-			// p2[1] = p1[1];
-			// p2[2] = p1[2];
-			// p2[3] = p1[3];
-			{
-				for (uint32_t i = 0; i < 4; i++ )
-					p2[i] = p1[0];
-			}
-	   }
-	   else {
-			uint8_t *p3 = (uint8_t *)&((&((tpm_t *)sealed_data)->seal_info)->tag);
-			// p3[0] = p1[0];
-			// p3[1] = p1[1];
-			{
-				for (uint32_t i = 0; i < 2; i++ )
-					p3[i] = p1[0];
-			}
+			uint8_t *p2 = (uint8_t *)&(
+				( (tpm_short_t *)sealed_data )->enc_data_size
+			);
+			for (uint32_t i = 0; i < 4; i++ )
+				p2[i] = p1[0];
+	   } else {
+			tpm_t *a = (tpm_t *)sealed_data;
+			uint8_t *p3 = (uint8_t *)&( ( (a->seal_info) ) .tag );
+			for (uint32_t i = 0; i < 2; i++ )
+				p3[i] = p1[0];
 	   }
 	}
 
