03d4ab4236b9e6ad9fca34e745d04e9ac6a68556
Eric Li <xiaoyili@andrew.cmu.edu>
2022-04-02 22:05:06 -0400

    Revert "Test after bug_061 is fixed"
    This reverts commit 724e436fff59ede20db0fd3812f15ed542633dd5.

--- xmhf/src/xmhf-core/include/arch/x86/_configx86.h
+++ xmhf/src/xmhf-core/include/arch/x86/_configx86.h
@@ -56,7 +56,7 @@
 //SL + runtime base addresses
 //SL currently sits at absolute address 256MB (0x10000000). runtime is
 //at an offset of 2M from this SL base address
-#define __TARGET_BASE_SL				0x20000000
+#define __TARGET_BASE_SL				0x10000000
 #define __TARGET_BASE					(__TARGET_BASE_SL + PAGE_SIZE_2M)
 
 //"sl" parameter block magic value
