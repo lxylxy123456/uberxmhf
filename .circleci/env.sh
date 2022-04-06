861f79c71edc043a4872748a9d9e4fc0115121f7
Eric Li <xiaoyili@andrew.cmu.edu>
2022-04-06 01:00:25 -0400

    env.sh

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ "$1" == "i386" ]
+set -xe
+if [ "$1" == "i386" ]; then
 	export SUBARCH="i386"
 	export SA_x86_x64="x86"
 	export SA_32_64="32"
-else if [ "$1" == "amd64" ]
+else if [ "$1" == "amd64" ]; then
 	export SUBARCH="amd64"
 	export SA_x86_x64="x64"
 	export SA_32_64="64"
