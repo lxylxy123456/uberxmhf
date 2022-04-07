8b076c462c30ca44489e897ead30088a9b96f708
Eric Li <xiaoyili@andrew.cmu.edu>
2022-04-06 22:24:36 -0400

    stderr

--- .circleci/test2.py
+++ .circleci/test2.py
@@ -57,7 +57,9 @@ def spawn_qemu(args, serial_file):
 	if args.no_display:
 		qemu_args.append('-display')
 		qemu_args.append('none')
-	p = Popen(qemu_args, stdin=-1, stdout=-1, stderr=-1)
+	p = Popen(qemu_args, stdin=-1, stdout=-1
+		#, stderr=-1
+		)
 	return p
 
 def get_ssh_cmd(args, ssh_port, exe='ssh', port_opt='-p'):
@@ -164,13 +166,13 @@ def ssh_operations(args, ssh_port):
 	print('scp')
 	check_call(scp_cmd)
 	print('scp done')
-#	# 3. install
-#	ss = [threading.Lock(), SSH_CONNECTING, 0, []]
-#	install_num = { 'i386': 86, 'amd64': 64 }[args.subarch]
-#	stat = run_ssh('date; echo 3. install; ./install%d.sh' % install_num,
-#					10, 20, ss)
-#	if stat or ss[2] != 0:
-#		return 'Failed to install: (%s, %d, %s)' % (stat, ss[2], ss[3])
+	# 3. install
+	ss = [threading.Lock(), SSH_CONNECTING, 0, []]
+	install_num = { 'i386': 86, 'amd64': 64 }[args.subarch]
+	stat = run_ssh('date; echo 3. install; ./install%d.sh' % install_num,
+					10, 20, ss)
+	if stat or ss[2] != 0:
+		return 'Failed to install: (%s, %d, %s)' % (stat, ss[2], ss[3])
 	# 4. restart
 	ss = [threading.Lock(), SSH_CONNECTING, 0, []]
 	stat = run_ssh('date; echo 4. restart; touch /tmp/asdf; sudo init 6; ',
