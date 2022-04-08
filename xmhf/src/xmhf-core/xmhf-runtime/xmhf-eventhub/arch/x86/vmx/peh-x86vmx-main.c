f3e1c154ee6c951f1f5bebf8d66e6e9bd9e5cc87
Eric Li <xiaoyili@andrew.cmu.edu>
2022-04-07 20:33:57 -0400

    Remove debug print

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-eventhub/arch/x86/vmx/peh-x86vmx-main.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-eventhub/arch/x86/vmx/peh-x86vmx-main.c
@@ -1212,7 +1212,6 @@ u32 xmhf_parteventhub_arch_x86vmx_intercept_handler(VCPU *vcpu, struct regs *r){
 		}
 	} //end switch((u32)vcpu->vmcs.info_vmexit_reason)
 
-	printf("{%d,%d}", vcpu->id, vcpu->vmcs.info_vmexit_reason);
 
 	/*
 	 * Check and clear guest interruptibility state.
