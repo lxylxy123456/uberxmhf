336240b04ccc4451fbd11a77f252aa4b9014966d
Eric Li <xiaoyili@andrew.cmu.edu>
2022-04-20 23:39:44 -0400

    Use monitor trap flag

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-eventhub/arch/x86/vmx/peh-x86vmx-main.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-eventhub/arch/x86/vmx/peh-x86vmx-main.c
@@ -960,9 +960,9 @@ u32 xmhf_parteventhub_arch_x86vmx_intercept_handler(VCPU *vcpu, struct regs *r){
 	 * is for quiescing (vcpu->vmcs.info_vmexit_reason == VMX_VMEXIT_EXCEPTION),
 	 * otherwise will deadlock. See xmhf_smpguest_arch_x86vmx_quiesce().
 	 */
-	if (vcpu->vmcs.info_vmexit_reason != VMX_VMEXIT_EXCEPTION) {
-		printf("{%d,%d}", vcpu->id, (u32)vcpu->vmcs.info_vmexit_reason);
-	}
+//	if (vcpu->vmcs.info_vmexit_reason != VMX_VMEXIT_EXCEPTION) {
+//		printf("{%d,%d}", vcpu->id, (u32)vcpu->vmcs.info_vmexit_reason);
+//	}
 
 	//handle intercepts
 	switch((u32)vcpu->vmcs.info_vmexit_reason){
@@ -1171,7 +1171,6 @@ u32 xmhf_parteventhub_arch_x86vmx_intercept_handler(VCPU *vcpu, struct regs *r){
 		break;
 
 		case 37: {
-			printf("\n");
 			printf("\nCPU(0x%02x): trapped at     %016llx", vcpu->id,
 					vcpu->vmcs.guest_RIP);
 			vcpu->vmcs.control_VMX_cpu_based &= ~(1 << 27);
