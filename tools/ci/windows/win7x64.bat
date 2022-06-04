9768f4d6f67ae926a3c23ab4d982d6d0c999ebd8
Eric Li <xiaoyili@andrew.cmu.edu>
2022-06-04 12:54:46 -0700

    Update win7x64.bat

--- tools/ci/windows/win7x64.bat
+++ tools/ci/windows/win7x64.bat
@@ -27,6 +27,27 @@ F:\test32.exe 1300000032
 F:\test32.exe 1300000032
 F:\test32.exe 1300000032
 
+:: Test amd64 pal_demo
+
+F:\test64.exe 1100000064
+F:\test64.exe 1100000064
+F:\test64.exe 1100000064
+
+F:\test_args64.exe 7 7 7
+
+IF %ERRORLEVEL% EQU 0 GOTO success64
+F:\test64.exe 1200000064
+F:\test64.exe 1200000064
+F:\test64.exe 1200000064
+F:\test64.exe 1444444444
+pause
+exit 1
+
+:success64
+F:\test64.exe 1300000064
+F:\test64.exe 1300000064
+F:\test64.exe 1300000064
+
 :: End test
 
 F:\test32.exe 1555555555
