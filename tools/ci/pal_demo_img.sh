846abf57bda82995bdd495a00382ecb450df1dbd
Eric Li <xiaoyili@andrew.cmu.edu>
2022-06-04 00:13:08 -0700

    debug

--- tools/ci/pal_demo_img.sh
+++ tools/ci/pal_demo_img.sh
@@ -6,7 +6,7 @@ DISK_FILE="$1"
 [ -n "$DISK_FILE" ]
 rm -f $DISK_FILE
 
-dd if=/dev/zero of="$DISK_FILE" bs=1M count=0 seek=2
+dd if=/dev/zero of="$DISK_FILE" bs=1M count=0 seek=32
 mformat -i "$DISK_FILE" ::
 
 for i in hypapps/trustvisor/pal_demo/{main,test,test_args}{32,64}{,.exe}; do
