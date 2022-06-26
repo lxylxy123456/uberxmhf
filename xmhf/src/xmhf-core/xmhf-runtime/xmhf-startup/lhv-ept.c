f89a73a1bd3b6022a6796519de19066e356cc55b
Eric Li <xiaoyili@andrew.cmu.edu>
2022-06-26 12:22:50 -0700

    Be able to run full LHV guest with EPT

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-ept.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-ept.c
@@ -50,6 +50,18 @@ static void* lhv_ept_pa2ptr(void *vctx, hpt_pa_t spa, size_t sz, hpt_prot_t acce
 	return spa2hva(spa);
 }
 
+static void ept_map_continuous_addr(VCPU *vcpu, lhv_ept_ctx_t *ept_ctx,
+									hpt_pmeo_t *pmeo, u64 low, u64 high)
+{
+	u64 paddr;
+	printf("CPU(0x%02x): ept map 0x%08llx - 0x%08llx\n", vcpu->id, low, high);
+	for (paddr = low; paddr < high; paddr += PA_PAGE_SIZE_4K) {
+		hpt_pmeo_set_address(pmeo, paddr);
+		HALT_ON_ERRORCOND(hptw_insert_pmeo_alloc(&ept_ctx->ctx, pmeo,
+												 paddr) == 0);
+	}
+}
+
 u64 lhv_build_ept(VCPU *vcpu)
 {
 	u64 low = rpb->XtVmmRuntimePhysBase;
@@ -58,7 +70,6 @@ u64 lhv_build_ept(VCPU *vcpu)
 #else /* !__SKIP_RUNTIME_BSS__ */
 	u64 high = low + rpb->XtVmmRuntimeSize;
 #endif /* __SKIP_RUNTIME_BSS__ */
-	u64 paddr;
 	lhv_ept_ctx_t ept_ctx;
 	hpt_pmeo_t pmeo;
 	/* Assuming that ept_pool and ept_alloc are initialized to 0 by bss */
@@ -75,12 +86,12 @@ u64 lhv_build_ept(VCPU *vcpu)
 	hpt_pmeo_setuser(&pmeo, true);
 	hpt_pmeo_setprot(&pmeo, HPT_PROTS_RWX);
 	hpt_pmeo_setcache(&pmeo, HPT_PMT_WB);
-	printf("CPU(0x%02x): low=0x%08llx, high=0x%08llx\n", vcpu->id, low, high);
-	for (paddr = low; paddr < high; paddr += PA_PAGE_SIZE_4K) {
-		hpt_pmeo_set_address(&pmeo, paddr);
-		HALT_ON_ERRORCOND(hptw_insert_pmeo_alloc(&ept_ctx.ctx, &pmeo,
-												 paddr) == 0);
-	}
+	/* Regular memory */
+	ept_map_continuous_addr(vcpu, &ept_ctx, &pmeo, low, high);
+	/* LAPIC */
+	ept_map_continuous_addr(vcpu, &ept_ctx, &pmeo, 0xfee00000, 0xfee01000);
+	/* Console */
+	ept_map_continuous_addr(vcpu, &ept_ctx, &pmeo, 0x000b8000, 0x000b9000);
 	return (uintptr_t)ept_root;
 }
 
