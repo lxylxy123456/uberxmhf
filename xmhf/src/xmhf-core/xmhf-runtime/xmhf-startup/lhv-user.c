94275e47a615915cdaed5fff0b98f3902a8b59e7
Eric Li <xiaoyili@andrew.cmu.edu>
2022-06-26 22:45:27 -0700

    Can run PAL

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-user.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-user.c
@@ -23,6 +23,8 @@ void enter_user_mode(VCPU *vcpu, ulong_t arg)
 #else
 	uintptr_t stack_top = ((uintptr_t) user_stack[vcpu->idx]) + PAGE_SIZE_4K;
 	u32 *stack = (u32 *)stack_top;
+	u32 i, j, k;
+	u64 paddr = 0;
 	ureg_t ureg = {
 		.edi=0,
 		.esi=0,
@@ -41,11 +43,31 @@ void enter_user_mode(VCPU *vcpu, ulong_t arg)
 	stack[-1] = arg;
 	stack[-2] = (uintptr_t) vcpu;
 	stack[-3] = 0xdeadbeef;
+	/* Setup page table */
+	for (i = 0; i < 4; i++) {
+		user_pdpt[vcpu->idx][i] = 1 | (uintptr_t) user_pd[vcpu->idx][i];
+		for (j = 0; j < 512; j++) {
+			user_pd[vcpu->idx][i][j] = 7 | (uintptr_t) user_pt[vcpu->idx][i][j];
+			for (k = 0; k < 512; k++) {
+				user_pt[vcpu->idx][i][j][k] = 7 | paddr;
+				paddr += PA_PAGE_SIZE_4K;
+			}
+		}
+	}
+	write_cr3((uintptr_t) user_pdpt[vcpu->idx]);
 	enter_user_mode_asm(&ureg);
 #endif
 }
 
 /* Below are for pal_demo */
+
+void begin_pal_c(void) {}
+
+uintptr_t my_pal(uintptr_t arg1) {
+	return arg1 + 0x1234abcd;
+}
+
+void end_pal_c(void) {}
 
 u8 pal_demo_code[MAX_VCPU_ENTRIES][PAGE_SIZE_4K]
 __attribute__(( section(".bss.palign_data") ));
@@ -77,16 +99,26 @@ void user_main(VCPU *vcpu, ulong_t arg)
 		}
 	};
 	struct tv_pal_params params = {
-		num_params: 2,
+		num_params: 1,
 		params: {
-			{ TV_PAL_PM_INTEGER, 4 },
-			{ TV_PAL_PM_POINTER, 4 }
+			{ TV_PAL_PM_INTEGER, 4 }
 		}
 	};
-	uintptr_t pal_entry = 0;	// TODO
+	/* Copy code */
+	uintptr_t src_begin = (uintptr_t) begin_pal_c;
+	uintptr_t dst_begin = (uintptr_t) pal_demo_code[vcpu->idx];
+	uintptr_t code_size = (uintptr_t) end_pal_c - src_begin;
+	uintptr_t pal_entry = dst_begin - src_begin + (uintptr_t) my_pal;
+	typeof(&my_pal) pal_func = (typeof(&my_pal)) pal_entry;
+	memcpy((void *)dst_begin, (void *)src_begin, code_size);
 	HALT_ON_ERRORCOND(!vmcall(
 		TV_HC_REG, (uintptr_t)&sections, 0, (uintptr_t)&params, pal_entry));
+	HALT_ON_ERRORCOND(pal_func(0x11111111) == 0x2345bcde);
 
-	while (1) printf(".");
+	for (u32 i = 0; ; i++) {
+		if (i == 0) {
+			printf("ended\n");
+		}
+	}
 	(void)arg;
 }
