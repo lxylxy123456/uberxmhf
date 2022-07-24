a770d519a57b0202ebbb3ad10449cc70e25d8b9f
Eric Li <xiaoyili@andrew.cmu.edu>
2022-07-23 21:06:18 -0700

    Fix bug in experiment_13()

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-guest.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-guest.c
@@ -725,7 +725,9 @@ static void experiment_12_vmcall(void)
 /*
  * Experiment 13: NMI Exiting = 1, virtual NMIs = 1
  * L1 (host) blocks NMI. L2 (guest) does not block NMI. L1 VMENTRY to L2, then
- * L2 VMEXIT to L1. Then L1 expects an interrupt. Result: L1 does not get NMI.
+ * L2 VMEXIT to L1. Then L1 expects an interrupt. Result: L1 gets NMI.
+ * This test does not work on Bochs.
+ * This test does not work on QEMU.
  */
 static void experiment_13(void)
 {
@@ -746,9 +748,9 @@ static void experiment_13_vmcall(void)
 		break;
 	case 1:
 		TEST_ASSERT(!get_blocking_by_nmi());
-		hlt_wait(EXIT_TIMER_H);
-		iret_wait(EXIT_NMI_H);
+		hlt_wait(EXIT_NMI_H);
 		iret_wait(EXIT_MEASURE);
+		hlt_wait(EXIT_TIMER_H);
 		break;
 	default:
 		TEST_ASSERT(0 && "unexpected state");
@@ -776,7 +778,7 @@ static struct {
 	{experiment_10, experiment_10_vmcall, true, true, false},
 	{experiment_11, experiment_11_vmcall, true, true, false},
 	{experiment_12, experiment_12_vmcall, true, true, true},
-	{experiment_13, experiment_13_vmcall, true, true, true},
+	{experiment_13, experiment_13_vmcall, true, false, false},
 };
 
 static u32 nexperiments = sizeof(experiments) / sizeof(experiments[0]);
@@ -825,7 +827,7 @@ void lhv_guest_main(ulong_t cpu_id)
 		}
 	}
 	{
-		// experiment_13();
+		experiment_13();
 	}
 	if (1 && "random") {
 		for (u32 i = 0; i < 100000; i++) {
