787b7d2367466893ad4dbe8d9a08f201debe439c
Eric Li <xiaoyili@andrew.cmu.edu>
2022-08-08 00:52:16 -0700

    Use test5.py from xmhf64-nest to test LHV

--- tools/ci/test3.py
+++ tools/ci/test3.py
@@ -17,31 +17,20 @@ println_lock = threading.Lock()
 def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--subarch', required=True)
-	parser.add_argument('--qemu-image')
-	parser.add_argument('--qemu-image-back')
+	parser.add_argument('--xmhf-img', required=True)
+	parser.add_argument('--debian-img', required=True)
 	parser.add_argument('--smp', type=int, default=2)
 	parser.add_argument('--work-dir', required=True)
 	parser.add_argument('--no-display', action='store_true')
 	parser.add_argument('--sshpass', help='Password for ssh')
 	parser.add_argument('--verbose', action='store_true')
 	parser.add_argument('--watch-serial', action='store_true')
-	parser.add_argument('--skip-reset-qemu', action='store_true')
 	args = parser.parse_args()
 	return args
 
 def println(*args):
 	with println_lock:
 		print('{', *args, '}')
-
-def reset_qemu(args):
-	'''
-	Remove changes relative to backing qcow2 file
-	'''
-	assert os.path.exists(args.qemu_image)
-	if not os.path.exists(args.qemu_image_back):
-		raise Exception('reset_qemu() requires --qemu-image-back.')
-	check_call(['qemu-img', 'create', '-f', 'qcow2', '-b', args.qemu_image_back,
-				'-F', 'qcow2', args.qemu_image])
 
 def get_port():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
@@ -56,11 +45,11 @@ def get_port():
 	else:
 		raise RuntimeError('Cannot get port')
 
-def spawn_qemu(args, xmhf_img, serial_file, ssh_port):
+def spawn_qemu(args, serial_file, ssh_port):
 	qemu_args = [
 		'qemu-system-x86_64', '-m', '512M',
-		'--drive', 'media=disk,file=%s,format=raw,index=1' % xmhf_img,
-#		'--drive', 'media=disk,file=%s,format=qcow2,index=2' % args.qemu_image,
+		'--drive', 'media=disk,file=%s,format=raw,index=0' % args.xmhf_img,
+		'--drive', 'media=disk,file=%s,format=qcow2,index=1' % args.debian_img,
 		'-device', 'e1000,netdev=net0',
 		'-netdev', 'user,id=net0,hostfwd=tcp::%d-:22' % ssh_port,
 		'-smp', str(args.smp), '-cpu', 'Haswell,vmx=yes', '--enable-kvm',
@@ -72,6 +61,7 @@ def spawn_qemu(args, xmhf_img, serial_file, ssh_port):
 	popen_stderr = { 'stderr': -1 }
 	if args.verbose:
 		del popen_stderr['stderr']
+		print(' '.join(qemu_args))
 	p = Popen(qemu_args, stdin=-1, stdout=-1, **popen_stderr)
 	return p
 
@@ -178,24 +168,19 @@ class SSHOperations:
 		wordsizes = { 'i386': [32], 'amd64': [32, 64] }[self.args.subarch]
 		for w in wordsizes:
 			ss = []
-			#cmd = 'date; echo 7. run test %d; ./test_args%d 7 7 7' % (w, w)
-			cmd = 'date; echo 7. run test %d; echo ./test_args%d 7 7 7' % (w, w)
+			cmd = 'date; echo 7. run test %d; ./test_args%d 7 7 7' % (w, w)
 			stat = self.run_ssh(cmd, 10, 45, ss)
-			#if stat or ss[2] != 0 or 'Test pass' not in ss[3]:
-			if stat or ss[2] != 0:
+			if stat or ss[2] != 0 or 'Test pass' not in ss[3]:
 				return 'Test %d failed: (%s, %d, %s)' % (w, stat, ss[2], ss[3])
 		# Success
 		return None
 
 def main():
 	args = parse_args()
-	if not args.skip_reset_qemu:
-		reset_qemu(args)
 	ssh_port = get_port()
 	println('Use ssh port', ssh_port)
 	serial_file = os.path.join(args.work_dir, 'serial')
-	xmhf_img = os.path.join(args.work_dir, 'grub/c.img')
-	p = spawn_qemu(args, xmhf_img, serial_file, ssh_port)
+	p = spawn_qemu(args, serial_file, ssh_port)
 
 	# Simple workaround to watch serial output
 	if args.watch_serial:
@@ -204,19 +189,14 @@ def main():
 
 	result = 'Unknown'
 	try:
-#		result = SSHOperations(args, ssh_port).ssh_operations()
-#		if result is None:
-#			# give the OS 10 seconds to shutdown; if wait() succeeds, calling
-#			# wait() again will still succeed
-#			try:
-#				p.wait(timeout=HALT_TIMEOUT)
-#			except subprocess.TimeoutExpired:
-#				pass
-		for i in range(10):
-			println('MET:', i)
-			time.sleep(1)
-		# OS cannot be started, always pass
-		result = None
+		result = SSHOperations(args, ssh_port).ssh_operations()
+		if result is None:
+			# give the OS 10 seconds to shutdown; if wait() succeeds, calling
+			# wait() again will still succeed
+			try:
+				p.wait(timeout=HALT_TIMEOUT)
+			except subprocess.TimeoutExpired:
+				pass
 	finally:
 		p.kill()
 		p.wait()
@@ -229,10 +209,10 @@ def main():
 		return 1
 
 	# Test serial output
-	println('Test LHV banner in serial')
-	check_call(['grep', 'Lightweight Hypervisor', serial_file])
-	println('Test APs')
-	check_call(['grep', 'APs all awake', serial_file])
+	println('Test XMHF banner in serial')
+	check_call(['grep', 'eXtensible Modular Hypervisor', serial_file])
+	println('Test E820 in serial')
+	check_call(['grep', 'e820', serial_file])
 
 	println('TEST PASSED')
 	return 0
