c30fef4f954b9949784b57c0d98753f7e02c3f27
Eric Li <xiaoyili@andrew.cmu.edu>
2022-09-02 13:24:43 -0400

    Disallow 64-bit in lhv-guest.c

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-guest.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-guest.c
@@ -4,6 +4,10 @@
 
 #define INTERRUPT_PERIOD 10
 
+#ifndef __I386__
+#error "LHV NMI experiments only support 32-bit."
+#endif
+
 /*
  * An interrupt handler or VMEXIT handler will see exit_source. If it sees
  * other than EXIT_IGNORE or EXIT_MEASURE, it will error. If it sees
@@ -1596,73 +1600,6 @@ void lhv_vmcall_main(void)
 	experiments[experiment_no].vmcall();
 	printf("  Leave host\n");
 }
-
-#if 0
-void lhv_guest_main_old(ulong_t cpu_id)
-{
-	VCPU *vcpu = _svm_and_vmx_getvcpu();
-	console_vc_t vc;
-	HALT_ON_ERRORCOND(cpu_id == vcpu->idx);
-	console_get_vc(&vc, vcpu->idx, 1);
-	console_clear(&vc);
-	for (int i = 0; i < vc.width; i++) {
-		for (int j = 0; j < 2; j++) {
-			HALT_ON_ERRORCOND(console_get_char(&vc, i, j) == ' ');
-			console_put_char(&vc, i, j, '0' + vcpu->id);
-		}
-	}
-	if (!(__LHV_OPT__ & LHV_NO_EFLAGS_IF)) {
-		asm volatile ("sti");
-	}
-	while (1) {
-		if (!(__LHV_OPT__ & LHV_NO_EFLAGS_IF)) {
-			asm volatile ("hlt");
-		}
-		asm volatile ("vmcall");
-		if (__LHV_OPT__ & LHV_USE_EPT) {
-			u32 a = 0xdeadbeef;
-			u32 *p = (u32 *)0x12340000;
-			asm volatile("movl (%1), %%eax" :
-						 "+a" (a) :
-						 "b" (p) :
-						 "cc", "memory");
-			if (0) {
-				printf("CPU(0x%02x): EPT result: 0x%08x 0x%02x\n", vcpu->id, a,
-					   vcpu->ept_num);
-			}
-			if (vcpu->ept_num == 0) {
-				HALT_ON_ERRORCOND(a == 0xfee1c0de);
-			} else {
-				HALT_ON_ERRORCOND((u8) a == vcpu->ept_num);
-				HALT_ON_ERRORCOND((u8) (a >> 8) == vcpu->ept_num);
-				HALT_ON_ERRORCOND((u8) (a >> 16) == vcpu->ept_num);
-				HALT_ON_ERRORCOND((u8) (a >> 24) == vcpu->ept_num);
-			}
-		}
-		if (__LHV_OPT__ & LHV_USE_UNRESTRICTED_GUEST) {
-#ifdef __AMD64__
-			extern void lhv_disable_enable_paging(char *);
-			if ("quiet") {
-				lhv_disable_enable_paging("");
-			} else {
-				lhv_disable_enable_paging("LHV guest can disable paging\n");
-			}
-#elif defined(__I386__)
-			ulong_t cr0 = read_cr0();
-			asm volatile ("cli");
-			write_cr0(cr0 & 0x7fffffffUL);
-			if (0) {
-				printf("CPU(0x%02x): LHV guest can disable paging\n", vcpu->id);
-			}
-			write_cr0(cr0);
-			asm volatile ("sti");
-#else /* !defined(__I386__) && !defined(__AMD64__) */
-    #error "Unsupported Arch"
-#endif /* !defined(__I386__) && !defined(__AMD64__) */
-		}
-	}
-}
-#endif
 
 void lhv_guest_xcphandler(uintptr_t vector, struct regs *r)
 {
