ebd643440e7d9fd15f079d40fb5fc6aba48332e1
Eric Li <xiaoyili@andrew.cmu.edu>
2022-09-14 12:41:19 -0400

    Bug is reproducible in start of lhv_main()

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-timer.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv-timer.c
@@ -128,6 +128,7 @@ void handle_timer_interrupt(VCPU *vcpu, int vector, int guest)
 		return;
 	}
 	printf("Timer\n");
+	HALT();
 	if (vector == 0x20) {
 		vcpu->pit_time++;
 		update_screen(vcpu, &vcpu->lhv_pit_x[guest], 0, guest);
