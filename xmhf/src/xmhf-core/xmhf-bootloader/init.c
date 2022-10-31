lxylxy123456/uberxmhf

commit fd9e3e3bbe04d9d17f68c130b7048e25d73612f3

Add check to overlap of multiboot modules

xmhf/src/xmhf-core/xmhf-bootloader/init.c
@@ -1089,6 +1089,10 @@ void cstartup(multiboot_info_t *mbi){
     //deal with MP and get CPU table
     dealwithMP();
 
+    //check number of elements in mod_array. Currently bootloader assumes that
+    //mod_array[0] is SL+RT, mod_array[1] is guest OS boot module.
+    HALT_ON_ERRORCOND(mods_count >= 2);
+
     //find highest 2MB aligned physical memory address that the hypervisor
     //binary must be moved to
     sl_rt_size = mod_array[0].mod_end - mod_array[0].mod_start;
@@ -1101,6 +1105,21 @@ void cstartup(multiboot_info_t *mbi){
 #endif /* __SKIP_RUNTIME_BSS__ */
 
     hypervisor_image_baseaddress = dealwithE820(mbi, PAGE_ALIGN_UP_2M((sl_rt_size)));
+
+    //check whether multiboot modules overlap with SL+RT. mod_array[0] can
+    //overlap because we will use memmove() instead of memcpy(). Currently
+    //will panic if other mod_array[i] overlaps with SL+RT.
+    {
+        u32 i;
+        u32 sl_rt_start = hypervisor_image_baseaddress;
+        u32 sl_rt_end;
+        HALT_ON_ERRORCOND(!plus_overflow_u32(sl_rt_start, sl_rt_size));
+        sl_rt_end = sl_rt_start + sl_rt_size;
+        for(i=1; i < mods_count; i++) {
+			HALT_ON_ERRORCOND(mod_array[i].mod_start >= sl_rt_end ||
+			                  sl_rt_start >= mod_array[i].mod_end);
+        }
+    }
 
     //relocate the hypervisor binary to the above calculated address
     memmove((void*)hypervisor_image_baseaddress, (void*)mod_array[0].mod_start, sl_rt_size);

commit 83723a5197df6f373463b41b708c22331c59f72f

Fix regression caused by fd9e3e3bb

xmhf/src/xmhf-core/xmhf-bootloader/init.c
@@ -1056,7 +1056,8 @@ void cstartup(multiboot_info_t *mbi){
 
     //check number of elements in mod_array. Currently bootloader assumes that
     //mod_array[0] is SL+RT, mod_array[1] is guest OS boot module.
-    HALT_ON_ERRORCOND(mods_count >= 2);
+    HALT_ON_ERRORCOND(mods_count >= 1);
+    // HALT_ON_ERRORCOND(mods_count >= 2);
 
     //find highest 2MB aligned physical memory address that the hypervisor
     //binary must be moved to
@@ -1130,8 +1131,8 @@ void cstartup(multiboot_info_t *mbi){
         //memcpy((void *)&slpb->pcpus, (void *)&pcpus, (sizeof(PCPU) * pcpus_numentries));
         memcpy((void *)&slpb->cpuinfobuffer, (void *)&pcpus, (sizeof(PCPU) * pcpus_numentries));
         slpb->runtime_size = (mod_array[0].mod_end - mod_array[0].mod_start) - PAGE_SIZE_2M;
-        slpb->runtime_osbootmodule_base = mod_array[1].mod_start;
-        slpb->runtime_osbootmodule_size = (mod_array[1].mod_end - mod_array[1].mod_start);
+        slpb->runtime_osbootmodule_base = 0; // mod_array[1].mod_start;
+        slpb->runtime_osbootmodule_size = 0; // (mod_array[1].mod_end - mod_array[1].mod_start);
         slpb->runtime_osbootdrive = get_tboot_boot_drive();
 
 		//check if we have an optional app module and if so populate relevant SLPB