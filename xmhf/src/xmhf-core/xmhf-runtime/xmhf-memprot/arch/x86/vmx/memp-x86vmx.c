cd31b87f148b9415252bc8e65959d95175abe3a0
Eric Li <xiaoyili@andrew.cmu.edu>
2022-11-28 00:16:17 -0500

    Fix bug caused by 0051af01e75b47a7a533c8c476e91f9e6a258260

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86/vmx/memp-x86vmx.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86/vmx/memp-x86vmx.c
@@ -691,11 +691,6 @@ void xmhf_memprot_arch_x86vmx_set_EPTP(VCPU *vcpu, u64 eptp)
 u64 xmhf_memprot_arch_x86vmx_get_EPTP01(VCPU *vcpu)
 {
   HALT_ON_ERRORCOND(vcpu->cpu_vendor == CPU_VENDOR_INTEL);
-  /*
-   * Changing EPT01 is not supported for now. Note that changing another CPU's
-   * EPT01 can easily cause race conditions.
-   */
-  HALT_ON_ERRORCOND(0 && "disabled");
   return vcpu->vmcs.control_EPT_pointer;
 }
 
@@ -709,6 +704,11 @@ u64 xmhf_memprot_arch_x86vmx_get_EPTP01(VCPU *vcpu)
  */
 void xmhf_memprot_arch_x86vmx_set_EPTP01(VCPU *vcpu, u64 eptp)
 {
+  /*
+   * Changing EPT01 is not supported for now. Note that changing another CPU's
+   * EPT01 can easily cause race conditions.
+   */
+  HALT_ON_ERRORCOND(0 && "disabled");
   HALT_ON_ERRORCOND(vcpu->cpu_vendor == CPU_VENDOR_INTEL);
   vcpu->vmcs.control_EPT_pointer = eptp;
 }

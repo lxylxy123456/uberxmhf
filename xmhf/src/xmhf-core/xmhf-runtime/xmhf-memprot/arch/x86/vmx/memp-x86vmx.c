lxylxy123456/uberxmhf

commit 0051af01e75b47a7a533c8c476e91f9e6a258260

Disable hpt_emhf_set_l1_root_pm()

xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86/vmx/memp-x86vmx.c
@@ -691,6 +691,11 @@ void xmhf_memprot_arch_x86vmx_set_EPTP(VCPU *vcpu, u64 eptp)
 u64 xmhf_memprot_arch_x86vmx_get_EPTP01(VCPU *vcpu)
 {
   HALT_ON_ERRORCOND(vcpu->cpu_vendor == CPU_VENDOR_INTEL);
+  /*
+   * Changing EPT01 is not supported for now. Note that changing another CPU's
+   * EPT01 can easily cause race conditions.
+   */
+  HALT_ON_ERRORCOND(0 && "disabled");
   return vcpu->vmcs.control_EPT_pointer;
 }
 

xmhf/src/libbaremetal/libxmhfutil/include/hpt_emhf.h
@@ -161,6 +161,8 @@ static inline hpt_pa_t hpt_emhf_get_l1_root_pm_pa(VCPU *vcpu)
 static inline void hpt_emhf_set_l1_root_pm_pa(VCPU *vcpu, hpt_pa_t root_pa)
 {
   hpt_type_t t = hpt_emhf_get_hpt_type(vcpu);
+  /* Changing EPT01 is not supported for now */
+  HALT_ON_ERRORCOND(0 && "disabled");
   if (vcpu->cpu_vendor == CPU_VENDOR_INTEL) {
     xmhf_memprot_arch_x86vmx_set_EPTP01( vcpu, hpt_eptp_set_address(t,
                                                                     xmhf_memprot_arch_x86vmx_get_EPTP01(vcpu),
@@ -179,6 +181,8 @@ static inline hpt_pm_t hpt_emhf_get_l1_root_pm(VCPU *vcpu)
 
 static inline void hpt_emhf_set_l1_root_pm(VCPU *vcpu, hpt_pm_t root)
 {
+  /* Changing EPT01 is not supported for now */
+  HALT_ON_ERRORCOND(0 && "disabled");
   hpt_emhf_set_l1_root_pm_pa( vcpu, hva2spa(root));
 }
 

commit cd31b87f148b9415252bc8e65959d95175abe3a0

Fix bug caused by 0051af01e75b47a7a533c8c476e91f9e6a258260

xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86/vmx/memp-x86vmx.c
@@ -691,11 +691,6 @@ void xmhf_memprot_arch_x86vmx_set_EPTP(VCPU *vcpu, u64 eptp)
 u64 xmhf_memprot_arch_x86vmx_get_EPTP01(VCPU *vcpu)
 {
   HALT_ON_ERRORCOND(vcpu->cpu_vendor == CPU_VENDOR_INTEL);
-  /*
-   * Changing EPT01 is not supported for now. Note that changing another CPU's
-   * EPT01 can easily cause race conditions.
-   */
-  HALT_ON_ERRORCOND(0 && "disabled");
   return vcpu->vmcs.control_EPT_pointer;
 }
 
@@ -709,6 +704,11 @@ u64 xmhf_memprot_arch_x86vmx_get_EPTP01(VCPU *vcpu)
  */
 void xmhf_memprot_arch_x86vmx_set_EPTP01(VCPU *vcpu, u64 eptp)
 {
+  /*
+   * Changing EPT01 is not supported for now. Note that changing another CPU's
+   * EPT01 can easily cause race conditions.
+   */
+  HALT_ON_ERRORCOND(0 && "disabled");
   HALT_ON_ERRORCOND(vcpu->cpu_vendor == CPU_VENDOR_INTEL);
   vcpu->vmcs.control_EPT_pointer = eptp;
 }