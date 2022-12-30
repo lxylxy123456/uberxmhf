dba0a2e7f85485631b378c6da79f58fc7cdbad83
Eric Li <xiaoyili@andrew.cmu.edu>
2022-12-29 21:07:21 -0500

    Reproduce KVM instruction emulation bug

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-startup/lhv.c
@@ -1,7 +1,94 @@
 #include <xmhf.h>
 #include <lhv.h>
 
+//#define MOD 100000
+#define MOD 1
+volatile uintptr_t val = 0;
+//volatile uintptr_t *ptr = &val;
+volatile uintptr_t *ptr = (volatile uintptr_t *) 0xb8000;
+volatile bool flag = false;
+u64 count0 = 0;
+u64 count1 = 0;
+u64 count2 = 0;
+
 void lhv_main(VCPU *vcpu)
+{
+	{
+		static bool ready = false;
+		if (vcpu->isbsp) {
+			mb();
+			*ptr = 0;
+			mb();
+			ready = true;
+			mb();
+		} else {
+			while (!ready) {
+				xmhf_cpu_relax();
+			}
+		}
+	}
+	switch (vcpu->idx) {
+	case 0:
+		while (1) {
+			uintptr_t ax = 0x12345678U;
+			flag = true;
+			mb();
+			asm volatile("xchg %0, %1" : "+r"(ax), "+m"(*ptr));
+			mb();
+			HALT_ON_ERRORCOND(ax == 0);
+			count0++;
+			mb();
+			while (flag) {
+				xmhf_cpu_relax();
+			}
+			mb();
+			if (count0 % MOD == 0) {
+				printf("counts: %llu = %llu - (%llu = %llu + %llu)\n",
+					   count0 - (count1 + count2), count0, count1 + count2,
+					   count1, count2);
+			}
+			mb();
+		}
+		break;
+	case 1:
+		while (1) {
+			uintptr_t ax = 0;
+			asm volatile("xchg %0, %1" : "+r"(ax), "+m"(*ptr));
+			mb();
+			if (ax) {
+				mb();
+				count1++;
+				mb();
+				flag = false;
+				mb();
+			} else {
+				xmhf_cpu_relax();
+			}
+		}
+		break;
+	case 2:
+		while (1) {
+			uintptr_t ax = 0;
+			asm volatile("xchg %0, %1" : "+r"(ax), "+m"(*ptr));
+			mb();
+			if (ax) {
+				mb();
+				count2++;
+				mb();
+				flag = false;
+				mb();
+			} else {
+				xmhf_cpu_relax();
+			}
+		}
+		break;
+	default:
+		break;
+	}
+	HALT();
+}
+
+void lhv_main_old(VCPU *vcpu)
 {
 	console_vc_t vc;
 	console_get_vc(&vc, vcpu->idx, 0);
