3b1cd4bc2c45796e9bd41a508b4834576f97e946
Eric Li <xiaoyili@andrew.cmu.edu>
2023-01-01 13:44:54 -0500

    Implement access_memory_gv

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-eventhub/arch/x86/vmx/peh-x86vmx-emulation.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-eventhub/arch/x86/vmx/peh-x86vmx-emulation.c
@@ -51,35 +51,194 @@
 
 #define INST_LEN_MAX 15
 
+/* Select a segment that will be used to access memory */
+typedef enum cpu_segment_t {
+	CPU_SEG_ES,
+	CPU_SEG_CS,
+	CPU_SEG_SS,
+	CPU_SEG_DS,
+	CPU_SEG_FS,
+	CPU_SEG_GS,
+} cpu_segment_t;
+
+/* Environment used to access memory */
+typedef struct mem_access_env_t {
+	void *haddr;
+	gva_t gaddr;
+	cpu_segment_t seg;
+	size_t size;
+	hpt_prot_t mode;
+	hptw_cpl_t cpl;
+} mem_access_env_t;
+
 /*
- * Read memory from virtual address space.
- * For normal memory, exec=false. For instruction execution, exec=true.
+ * Given a segment index, translate logical address to linear address.
+ * seg: index of segment used by hardware.
+ * addr: logical address as input, modified to linear address as output.
+ * size: size of access.
+ * mode: access mode (read / write / execute). Use HPT_PROT_*_MASK macros.
+ * cpl: permission of access.
+ *
+ * Note: currently XMHF halts when guest makes an invalid access. Ideally XMHF
+ * should inject corresponding exceptions like #GP(0).
+ *
+ * TODO: merge this function with nested-x86vmx-handler1.c
  */
-static void read_memory_gv(guestmem_hptw_ctx_pair_t *ctx_pair, hptw_cpl_t cpl,
-						   void *dst, hpt_va_t src, size_t len, bool exec)
+static gva_t _vmx_decode_seg(VCPU * vcpu, cpu_segment_t seg, gva_t addr,
+							 size_t size, hpt_prot_t mode, hptw_cpl_t cpl)
 {
-	VCPU *vcpu = ctx_pair->vcpu;
-	memprot_x86vmx_eptlock_read_lock(vcpu);
-
-	/* TODO: Segmentation: logical address -> linear address */
-	/* TODO: Paging: linear address -> physical address  */
-	//hptw_checked_copy_from_va(ctx, cpl, dst, src, len) == 0;
-
-	if (vcpu->vmcs.guest_CR0 & CR0_PG) {
-		guestmem_copy_gv2h(ctx_pair, cpl, dst, src, len);
+	/* Get segment fields from VMCS */
+	ulong_t base;
+	u32 access_rights;
+	u32 limit;
+	bool g64 = VCPU_g64(vcpu);
+	switch (seg) {
+	case CPU_SEG_ES:
+		base = vcpu->vmcs.guest_ES_base;
+		access_rights = vcpu->vmcs.guest_ES_access_rights;
+		limit = vcpu->vmcs.guest_ES_limit;
+		break;
+	case CPU_SEG_CS:
+		base = vcpu->vmcs.guest_CS_base;
+		access_rights = vcpu->vmcs.guest_CS_access_rights;
+		limit = vcpu->vmcs.guest_CS_limit;
+		break;
+	case CPU_SEG_SS:
+		base = vcpu->vmcs.guest_SS_base;
+		access_rights = vcpu->vmcs.guest_SS_access_rights;
+		limit = vcpu->vmcs.guest_SS_limit;
+		break;
+	case CPU_SEG_DS:
+		base = vcpu->vmcs.guest_DS_base;
+		access_rights = vcpu->vmcs.guest_DS_access_rights;
+		limit = vcpu->vmcs.guest_DS_limit;
+		break;
+	case CPU_SEG_FS:
+		base = vcpu->vmcs.guest_FS_base;
+		access_rights = vcpu->vmcs.guest_FS_access_rights;
+		limit = vcpu->vmcs.guest_FS_limit;
+		break;
+	case CPU_SEG_GS:
+		base = vcpu->vmcs.guest_GS_base;
+		access_rights = vcpu->vmcs.guest_GS_access_rights;
+		limit = vcpu->vmcs.guest_GS_limit;
+		break;
+	default:
+		HALT_ON_ERRORCOND(0 && "Unexpected segment");
 	}
-	memprot_x86vmx_eptlock_read_unlock(vcpu);
+	/*
+	 * For 32-bit guest, check segment limit.
+	 * For 64-bit guest, no limit check is performed (can even wrap around).
+	 */
+	if (!g64) {
+		gva_t addr_max;
+		HALT_ON_ERRORCOND(addr <= UINT_MAX);
+		if (addr > UINT_MAX - size) {
+			HALT_ON_ERRORCOND(0 && "Invalid access: logical address overflow");
+		}
+		addr_max = addr + size;
+		if (addr_max >= limit) {
+			HALT_ON_ERRORCOND(0 && "Invalid access: segment limit exceed");
+		}
+		if (base > UINT_MAX - addr_max) {
+			HALT_ON_ERRORCOND(0 && "Not implemented: linear address overflow");
+		}
+	}
+	/* Check access rights. Skip checking if amd64 SS/DS/ES/FS/GS. */
+	if (seg == 1 || !g64) {
+		/* Check Segment type */
+		{
+			hpt_prot_t supported_modes = HPT_PROTS_NONE;
+			if ((access_rights & (1U << 3)) == 0) {
+				/* type = 0 - 7, always has read */
+				supported_modes |= HPT_PROT_READ_MASK;
+				if (access_rights & (1U << 1)) {
+					/* type = 2/3/6/7, has write */
+					supported_modes |= HPT_PROT_WRITE_MASK;
+				}
+				if (access_rights & (1U << 2)) {
+					/* type = 4 - 7, expand-down data segment not implemented */
+					HALT_ON_ERRORCOND(0 && "Not implemented: expand-down");
+				}
+			} else {
+				/* type = 8 - 15, always has execute */
+				supported_modes |= HPT_PROT_EXEC_MASK;
+				if (access_rights & (1U << 1)) {
+					/* type = 10/11/14/15, has read */
+					supported_modes |= HPT_PROT_READ_MASK;
+				}
+			}
+			if ((supported_modes & mode) != mode) {
+				HALT_ON_ERRORCOND(0 && "Invalid access: segment type");
+			}
+		}
+		/* Check S - Descriptor type (0 = system; 1 = code or data) */
+		HALT_ON_ERRORCOND((access_rights & (1U << 4)));
+		/* Check DPL - Descriptor privilege level */
+		{
+			u32 dpl = (access_rights >> 5) & 0x3;
+			if (dpl < (u32) cpl) {
+				HALT_ON_ERRORCOND(0 && "Invalid access: DPL");
+			}
+		}
+		/* Check P - Segment present */
+		HALT_ON_ERRORCOND((access_rights & (1U << 7)));
+	}
+	return addr + base;
 }
 
-static void write_memory_gv(guestmem_hptw_ctx_pair_t *ctx_pair, hptw_cpl_t cpl,
-							hpt_va_t dst, void *src, size_t len)
+/* Access memory from guest logical address. */
+static void access_memory_gv(guestmem_hptw_ctx_pair_t * ctx_pair,
+							 mem_access_env_t * env)
 {
-	(void)ctx_pair;
-	(void)cpl;
-	(void)dst;
-	(void)src;
-	(void)len;
-	HALT_ON_ERRORCOND(0 && "TODO");
+	VCPU *vcpu = ctx_pair->vcpu;
+	hpt_va_t lin_addr;
+	size_t copied;
+	memprot_x86vmx_eptlock_read_lock(vcpu);
+
+	/* Segmentation: logical address -> linear address */
+	lin_addr = _vmx_decode_seg(vcpu, env->seg, env->gaddr, env->size, env->mode,
+							   env->cpl);
+
+	/* Paging */
+	copied = 0;
+	while (copied < env->size) {
+		hpt_va_t gva = lin_addr + copied;
+		size_t size = env->size - copied;
+		hpt_va_t gpa;
+		void *hva;
+
+		/* Linear address -> guest physical address */
+		if (vcpu->vmcs.guest_CR0 & CR0_PG) {
+			hpt_pmeo_t pmeo;
+			int ret = hptw_checked_get_pmeo(&pmeo, &ctx_pair->guest_ctx,
+										    env->mode, env->cpl, gva);
+			HALT_ON_ERRORCOND(ret == 0);
+			gpa = hpt_pmeo_va_to_pa(&pmeo, gva);
+			size = MIN(size, hpt_remaining_on_page(&pmeo, gpa));
+		} else {
+			gpa = gva;
+		}
+
+		/* Guest physical address -> hypervisor physical address */
+		hva = hptw_checked_access_va(&ctx_pair->host_ctx, env->mode, env->cpl,
+									 gpa, size, &size);
+		if (hva == NULL) {
+			/* Memory not in EPT, need special treatment */
+			HALT_ON_ERRORCOND(0);
+			copied += size;
+		} else {
+			/* Perform normal memory access */
+			if (env->mode & HPT_PROT_WRITE_MASK) {
+				memcpy(hva, env->haddr + copied, size);
+			} else {
+				memcpy(env->haddr + copied, hva, size);
+			}
+			copied += size;
+		}
+	}
+
+	memprot_x86vmx_eptlock_read_unlock(vcpu);
 }
 
 // TODO: doc
@@ -92,11 +251,16 @@ void emulate_instruction(VCPU * vcpu)
 		uintptr_t rip = vcpu->vmcs.guest_RIP;
 		u32 inst_len = vcpu->vmcs.info_vmexit_instruction_length;
 		HALT_ON_ERRORCOND(inst_len < INST_LEN_MAX);
-		// TODO: use read_memory_gv
-		if (vcpu->vmcs.guest_CR0 & CR0_PG) {
-			guestmem_copy_gv2h(&ctx_pair, 0, inst, rip, inst_len);
-		} else {
-			guestmem_copy_gp2h(&ctx_pair, 0, inst, rip, inst_len);
+		{
+			mem_access_env_t env = {
+				.haddr = inst,
+				.gaddr = rip,
+				.seg = CPU_SEG_CS,
+				.size = inst_len,
+				.mode = HPT_PROT_EXEC_MASK,
+				.cpl = vcpu->vmcs.guest_CS_selector & 3,
+			};
+			access_memory_gv(&ctx_pair, &env);
 		}
 		printf("CPU(0x%02x): emulation %d 0x%llx\n", vcpu->id, inst_len, *(u64 *)inst);
 	}
