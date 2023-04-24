8a82dc1587850920f8c0618599e8bbb0252a7446
Eric Li <xiaoyili@andrew.cmu.edu>
2023-04-23 23:00:26 -0400

    .

--- bug_071/main.c
+++ bug_071/main.c
@@ -1,6 +1,16 @@
 #include <efi.h>
 #include <efilib.h>
 
+#define CHK_EFI_ERROR(status) \
+	do { \
+		if (EFI_ERROR(status)) { \
+			Print(L"CHK_EFI_ERROR at %d failed with %r\n", __LINE__, status); \
+			while (1) { \
+				asm volatile("hlt"); \
+			} \
+		} \
+	} while(0)
+
 void debug_nop(void)
 {
 	(void)debug_nop;
@@ -12,8 +22,7 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
 	InitializeLib(ImageHandle, SystemTable);
 
-	Print(L"Hello, world!\n");
-	Print(L"%p!\n", efi_main);
+	Print(L"Hello, world from %p!\n", efi_main);
 
 	/* https://wiki.osdev.org/Debugging_UEFI_applications_with_GDB */
 	{
@@ -22,10 +31,7 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 		status = uefi_call_wrapper(ST->BootServices->HandleProtocol, 3,
 								   ImageHandle, &LoadedImageProtocol,
 								   (void **)&loaded_image);
-		if (EFI_ERROR(status)) {
-			Print(L"handleprotocol: %r\n", status);
-			// TODO
-		}
+		CHK_EFI_ERROR(status);
 		Print(L"Image base: 0x%lx\n", loaded_image->ImageBase);
 	}
 
@@ -34,12 +40,29 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
 	/* Look for serial device. */
 	{
-		//EFI_STATUS status;
-		//status = uefi_call_wrapper(ST->BootServices->LocateHandle, 4,)
-		//if (EFI_ERROR(status)) {
-		//	Print(L"handleprotocol: %r\n", status);
-		//	// TODO
-		//}
+		EFI_STATUS status;
+		UINTN NoHandles;
+		EFI_HANDLE *Buffer;
+		status = uefi_call_wrapper(ST->BootServices->LocateHandleBuffer, 4,
+								   ByProtocol, &SerialIoProtocol, NULL,
+								   &NoHandles, &Buffer);
+		CHK_EFI_ERROR(status);
+		Print(L"NoHandles: 0x%ld\n", NoHandles);
+		Print(L"Buffer: 0x%p\n", Buffer);
+
+		for (UINTN i = 0; i < NoHandles; i++) {
+			Print(L"Buffer[%ld]: 0x%p\n", i, Buffer[i]);
+			debug_nop();
+			{
+				EFI_STATUS status;
+				EFI_SERIAL_IO_PROTOCOL *Interface = NULL;
+				status = uefi_call_wrapper(ST->BootServices->HandleProtocol, 3
+										   Buffer[i], &SerialIoProtocol,
+										   (void **)&Interface);
+				CHK_EFI_ERROR(status);
+				Print(L"\tInterface: %p\n", Interface);
+			}
+		}
 	}
 
 	/* Prevent exiting, useful if not using EFI shell. */
