6f0544beb5b9585c5a5360a8ee3fadeaee59c891
Eric Li <xiaoyili@andrew.cmu.edu>
2023-04-25 20:46:02 -0400

    Output to serial port

--- bug_071/main.c
+++ bug_071/main.c
@@ -12,6 +12,30 @@
 		} \
 	} while(0)
 
+#define SERIAL_PORT (0x3f8)
+
+static inline void outb(uint8_t value, uint16_t port)
+{
+	__asm__ __volatile__ ("outb %b0,%w1": :"a" (value), "Nd" (port));
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+	unsigned char _v;
+	__asm__ __volatile__ ("inb %w1,%0":"=a" (_v):"Nd" (port));
+	return _v;
+}
+
+static void dbg_x86_uart_putc_bare(char ch){
+	//wait for xmit hold register to be empty
+	while ( ! (inb(SERIAL_PORT + 0x5) & 0x20) ) {
+		asm volatile("pause");
+	}
+
+	//write the character
+	outb(ch, SERIAL_PORT);
+}
+
 void debug_nop(void)
 {
 	(void)debug_nop;
@@ -24,6 +48,10 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 	InitializeLib(ImageHandle, SystemTable);
 
 	Print(L"Hello, world from %p!\n", efi_main);
+
+	for (char *s = "Hello, serial!\n"; *s != '\0'; s++) {
+		dbg_x86_uart_putc_bare(*s);
+	}
 
 	/* https://wiki.osdev.org/Debugging_UEFI_applications_with_GDB */
 	{
