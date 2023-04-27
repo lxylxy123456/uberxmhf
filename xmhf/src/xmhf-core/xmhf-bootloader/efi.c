5311035ae070c01c3f7ebeab16bc7f13d4408c57
Eric Li <xiaoyili@andrew.cmu.edu>
2023-04-26 20:10:25 -0400

    Be able to call other functions

--- xmhf/src/xmhf-core/xmhf-bootloader/efi.c
+++ xmhf/src/xmhf-core/xmhf-bootloader/efi.c
@@ -14,27 +14,7 @@
 
 #define SERIAL_PORT (0x3f8)
 
-static inline void outb(uint8_t value, uint16_t port)
-{
-	__asm__ __volatile__ ("outb %b0,%w1": :"a" (value), "Nd" (port));
-}
-
-static inline uint8_t inb(uint16_t port)
-{
-	unsigned char _v;
-	__asm__ __volatile__ ("inb %w1,%0":"=a" (_v):"Nd" (port));
-	return _v;
-}
-
-static void dbg_x86_uart_putc_bare(char ch){
-	//wait for xmit hold register to be empty
-	while ( ! (inb(SERIAL_PORT + 0x5) & 0x20) ) {
-		asm volatile("pause");
-	}
-
-	//write the character
-	outb(ch, SERIAL_PORT);
-}
+void dbg_x86_uart_putc(char ch);
 
 void debug_nop(void)
 {
@@ -50,7 +30,7 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 	Print(L"Hello, world from %p!\n", efi_main);
 
 	for (char *s = "Hello, serial!\n"; *s != '\0'; s++) {
-		dbg_x86_uart_putc_bare(*s);
+		dbg_x86_uart_putc(*s);
 	}
 
 	/* https://wiki.osdev.org/Debugging_UEFI_applications_with_GDB */
