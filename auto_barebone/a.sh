49e43fdfa2d9d510923c6716a0a0b58867687763
Eric Li <xiaoyili@andrew.cmu.edu>
2023-06-11 19:59:12 -0400

    Add custom build target in Makefile.am

--- auto_barebone/a.sh
+++ auto_barebone/a.sh
@@ -1,22 +1,9 @@
-# From https://wiki.osdev.org/Bare_Bones
-
 set -xe
 
-i686-elf-as() {
-	as --32 "$@"
-}
+autoreconf --install && ./configure && make
 
-i686-elf-gcc() {
-	gcc -m32 "$@"
-}
-
-i686-elf-as boot.s -o boot.o
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-
-mkdir -p isodir/boot/grub
-cp myos.bin isodir/boot/myos.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub2-mkrescue -o myos.iso isodir
+set +x
 
 echo qemu-system-i386 -cdrom myos.iso
+echo qemu-system-i386 -kernel myos.bin
+
